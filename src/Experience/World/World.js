31cec844a92bd1891f006ee69cfc6d7413cb6e7c
Renato1478 <renatoperesdesouza@hotmail.com>
2023-09-20 14:22:25 -0300

    Setting up main song and Song Controller + Ambient Light

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,29 +1,26 @@
-import Experience from '../Experience.js'
-import Environment from './Environment.js'
-import Floor from './Floor.js'
-import Fox from './Fox.js'
+import Experience from "../Experience.js";
+import Environment from "./Environment.js";
+import Floor from "./Floor.js";
+import Duck from "./Duck.js";
 
-export default class World
-{
-    constructor()
-    {
-        this.experience = new Experience()
-        this.scene = this.experience.scene
-        this.resources = this.experience.resources
+export default class World {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
 
-        // Wait for resources
-        this.resources.on('ready', () =>
-        {
-            // Setup
-            this.floor = new Floor()
-            this.fox = new Fox()
-            this.environment = new Environment()
-        })
+    // Wait for resources
+    this.resources.on("ready", () => {
+      // Setup
+      this.floor = new Floor();
+      this.duck = new Duck();
+      this.environment = new Environment();
+    });
+  }
+
+  update() {
+    if (this.duck) {
+      this.duck.update();
     }
-
-    update()
-    {
-        if(this.fox)
-            this.fox.update()
-    }
+  }
 }
--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -23,8 +23,8 @@ export default [
         path: 'textures/dirt/normal.jpg'
     },
     {
-        name: 'foxModel',
+        name: 'duckModel',
         type: 'gltfModel',
-        path: 'models/Fox/glTF/Fox.gltf'
+        path: 'models/Duck/glTF/Duck.gltf'
     }
 ]
