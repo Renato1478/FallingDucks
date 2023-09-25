5bca8497b0b7d0c601da5c46a759a169af29861e
Renato1478 <renatoperesdesouza@hotmail.com>
2023-09-24 23:59:41 -0300

    start button

--- src/Experience/Intro.js
+++ src/Experience/Intro.js
@@ -14,9 +14,9 @@ export default class Intro {
     this.setOverlay();
 
     /**
-     * Loading bar
+     * Loading info
      */
-    this.setLoadingBar();
+    this.setLoadingInfo();
   }
 
   setOverlay = () => {
@@ -45,29 +45,61 @@ export default class Intro {
     this.scene.add(this.overlay);
   };
 
-  setLoadingBar = () => {
+  removeOverlay = () => {
+    gsap.delayedCall(1.5, () => {
+      gsap.to(
+        this.overlayMaterial.uniforms.uAlpha,
+        3, // duration (seconds)
+        {
+          value: 0,
+        }
+      );
+    });
+  };
+
+  setLoadingInfo = () => {
+    // text
+    this.loadingTextElement = document.createElement("h2");
+    this.loadingTextElement.classList.add("loading-text");
+    this.loadingTextElement.innerHTML = "[0%]";
+    document.body.appendChild(this.loadingTextElement);
+    // bar
     this.loadingBarElement = document.createElement("div");
     this.loadingBarElement.classList.add("loading-bar");
     document.body.appendChild(this.loadingBarElement);
   };
 
+  removeLoadingInfo = () => {
+    // text
+    this.loadingTextElement.innerHTML = "Completed!";
+    this.loadingTextElement.classList.add("ended"); // 1.5s duration
+    // bar
+    this.loadingBarElement.classList.add("ended"); // 1.5s duration
+    this.loadingBarElement.style.transform = ``;
+  };
+
+  setStartButtonElement = () => {
+    this.startButtonElement = document.createElement("button");
+    this.startButtonElement.classList.add("start-button");
+    this.startButtonElement.innerHTML = "QUACK!";
+    this.startButtonElement.onclick = () => {
+      this.startButtonElement.classList.add("ended");
+      this.removeOverlay();
+    };
+    document.body.appendChild(this.startButtonElement);
+  };
+
   onProgress = (itemUrl, itemsLoaded, itemsTotal) => {
     const progressRatio = itemsLoaded / itemsTotal;
     this.loadingBarElement.style.transform = `scaleX(${progressRatio})`;
+    this.loadingTextElement.innerHTML = `[${Math.round(progressRatio * 100)}%]`;
   };
 
   onReady = () => {
     gsap.delayedCall(0.5, () => {
-      this.loadingBarElement.classList.add("ended"); // 1.5s duration
-      this.loadingBarElement.style.transform = ``;
+      this.removeLoadingInfo();
       gsap.delayedCall(1.5, () => {
-        gsap.to(
-          this.overlayMaterial.uniforms.uAlpha,
-          3, // duration (seconds)
-          {
-            value: 0,
-          }
-        );
+        this.setStartButtonElement();
       });
     });
   };
